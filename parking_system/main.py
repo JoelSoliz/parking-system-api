0fb026f908b585a7c729451acf591aeb21fe31fd
Joel Soliz <joelsolizchoque@gmail.com>
2023-04-11 06:44:20 -0400

    feat(user): Add get me generic function.

--- parking_system/main.py
+++ parking_system/main.py
@@ -1,9 +1,16 @@
 import os
+from typing import Union
 import uvicorn
-from fastapi import FastAPI
+from fastapi import Depends, FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 from api import administrator, auth, customer, employee, reservations, role, vehicle
+from api.dependencies import get_current_user
+from data.models import Administrator, Customer, Employee
+from schemas.administrator import Administrator as ASchema
+from schemas.customer import Customer as CSchema
+from schemas.employee import Employee as ESchema
+
 
 app = FastAPI()
 app.add_middleware(
@@ -25,6 +32,11 @@ app.include_router(vehicle.vehicle_router)
 @app.get("/")
 async def root():
     return {"message": "Welcome to Parking System API."}
+
+
+@app.get("/me", response_model=Union[ASchema, CSchema, ESchema], tags=["User"])
+def get_me(user: Union[Administrator, Customer, Employee] = Depends(get_current_user)):
+    return user
 
 
 if __name__ == "__main__":
