3f8ac946f8cd5fa2c5923db09461dcf11b26b486
Joel Soliz <joelsolizchoque@gmail.com>
2023-06-22 02:44:51 -0400

    hotfix(times): update default time function.

--- parking_system/data/models/reservation.py
+++ parking_system/data/models/reservation.py
@@ -1,7 +1,6 @@
-from datetime import datetime
-
-from sqlalchemy import Column, String, Boolean, Date, DateTime, ForeignKey
+from sqlalchemy import Column, String, Date, DateTime, ForeignKey
 from sqlalchemy.orm import relationship
+from sqlalchemy.sql import func
 
 from data import Base
 
@@ -11,15 +10,15 @@ class Reservation(Base):
     id_reservation = Column(String(4), primary_key=True)
     start_date = Column(Date(), nullable=False)
     end_date = Column(Date(), nullable=False)
-    create_at = Column(DateTime(), default=datetime.now())
+    create_at = Column(DateTime(), default=func.now())
     id_customer = Column(String(4), ForeignKey("customer.id_customer"))
-    id_price = Column(String(4), ForeignKey('hourly_rate.id_price'))
+    id_price = Column(String(4), ForeignKey("hourly_rate.id_price"))
 
     customer = relationship("Customer", back_populates="reservations")
     reservation_assignment = relationship("ReservationAssignment", back_populates="reservations")
     weekdays = relationship("WeekDay", back_populates="reservation")
     pays = relationship("Pay", back_populates="reservations")
-    price = relationship('HourlyRate', back_populates='reservations')
+    price = relationship("HourlyRate", back_populates="reservations")
 
     def __repr__(self):
         return f"<Reservation(id_reservation='{self.id_reservation}', start_date='{self.start_date}', end_date='{self.end_date}')>"
